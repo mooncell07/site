fca7c4959e801421c2ca9cdb84ee1e40fb511ed2
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-04-30 21:28:55 +0530

    feat: add +error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div class="error-content">
+    <div class="error-para">
+        <b><err>{$page.status}</err> {$page.error.message}</b>
+    </div>
+    <div class="error-emoticon">(¯―¯ ٥)</div>
+</div>
+
+<style>
+    err {
+        font-style: italic;
+        color: #f38ba8;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    anshu {
+      font-style: italic;
+      font-weight: bold;
+      color: #f38ba8;
+    }
+    anshu:before {
+      content: 'Anshu';
+    }
+</style>
+
+<div class="main-content">
+    <div class="main-emoticon">( ^_^)／</div>
+    <div class="main-para">
+        Hey everyone! My name's <anshu/> and i am a programmer from
+        <b><i>India</i></b>.<br />I love <b><i>emulation</i></b>, <b><i>networking</i></b> and also i
+        am highly interested in<br />
+        <b><i>web development!</i></b>
+    </div>
+    <div class="main-points">
+        <b>Currently learning:</b><br />
+        - Svelte, Reactjs<br />
+        - Rust
+    </div>
+</div>
+
