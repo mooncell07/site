b72793a8f582985271c38366c368389c3481c3fd
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-05-07 00:42:58 +0530

    feat: Add a really shitty implementation of projects page.

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,23 @@
+<style>
+
+.cards {
+    font-size: calc(3vw + 3vh);
+}
+
+</style>
+
+
+<div class="container">
+    <emoticon>˗ˏˋ ♡ ˎˊ˗</emoticon>
+    <p>Some of my projects on GitHub</p>
+    <div class="cards">
+    <a href="https://github.com/mooncell07/lemon"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=lemon&bg_color=f9e2af&title_color=181825&text_color=4c4f69" alt="mooncell07/lemon"> </a>
+    <a href="https://github.com/mooncell07/binary-river"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=binary-river&bg_color=89b4fa&title_color=181825&text_color=4c4f69" alt="mooncell07/binar-river"></a>
+    </div>
+
+    <div class="cards">
+    <a href="https://github.com/mooncell07/site"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=site&bg_color=cba6f7&title_color=181825&text_color=4c4f69" alt="mooncell07/binar-river"> </a>
+    <a href="https://github.com/mooncell07/Azaka"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=Azaka&bg_color=f5c2e7&title_color=181825&text_color=4c4f69" alt="mooncell07/binar-river"> </a>
+    </div>
+
+</div>
