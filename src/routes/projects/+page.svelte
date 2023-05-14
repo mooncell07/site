692a2aa8e8cf93d5dab8485f43269822df2fef05
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-05-14 18:56:45 +0530

    fix: Dynamically generate projects route

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,23 +1,85 @@
-<style>
+<script lang="ts">
+    const USER = "mooncell07";
+    let base = "https://github-readme-stats.vercel.app/api/pin/";
+    let defaults = {
+        username: USER,
+        title_color: "181825",
+        text_color: "4c4f69",
+        hide_border: true,
+        cache_seconds: "86400",
+    };
+    let prjcts = {
+        lemon: "f9e2af",
+        site: "cba6f7",
+        "binary-river": "89b4fa",
+        Azaka: "f5c2e7",
+    };
+    let cards: URL[] = [];
 
-.cards {
-    font-size: calc(3vw + 3vh);
-}
+    function applyDefaults(urlObj: URL) {
+        for (let i in defaults) {
+            urlObj.searchParams.set(
+                encodeURIComponent(i),
+                encodeURIComponent(defaults[i as keyof typeof defaults])
+            );
+        }
+    }
 
-</style>
+    function genCards() {
+        for (let p in prjcts) {
+            let urlObj = new URL(base);
+            applyDefaults(urlObj);
+            urlObj.searchParams.set("repo", p);
+            urlObj.searchParams.set(
+                "bg_color",
+                prjcts[p as keyof typeof prjcts]
+            );
+            cards.push(urlObj);
+        }
+    }
 
+    genCards();
+</script>
 
 <div class="container">
     <emoticon>˗ˏˋ ♡ ˎˊ˗</emoticon>
-    <p>Some of my projects on GitHub</p>
-    <div class="cards">
-    <a href="https://github.com/mooncell07/lemon"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=lemon&bg_color=f9e2af&title_color=181825&text_color=4c4f69&hide_border=true&cache_seconds=86400" alt="mooncell07/lemon"> </a>
-    <a href="https://github.com/mooncell07/binary-river"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=binary-river&bg_color=89b4fa&title_color=181825&text_color=4c4f69&hide_border=true&cache_seconds=86400" alt="mooncell07/binar-river"></a>
+    Some of my projects on GitHub
+    <div class="cardDiv">
+        {#each cards as card}
+            <p>
+                <a
+                    href="https://github.com/{USER}/{card.searchParams.get(
+                        'repo'
+                    )}"
+                >
+                    <img
+                        src={card.toString()}
+                        alt="mooncell07/{card.searchParams.get('repo')}"
+                    />
+                </a>
+            </p>
+        {/each}
     </div>
-
-    <div class="cards">
-    <a href="https://github.com/mooncell07/site"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=site&bg_color=cba6f7&title_color=181825&text_color=4c4f69&hide_border=true&cache_seconds=86400" alt="mooncell07/binar-river"> </a>
-    <a href="https://github.com/mooncell07/Azaka"><img src="https://github-readme-stats.vercel.app/api/pin/?username=mooncell07&repo=Azaka&bg_color=f5c2e7&title_color=181825&text_color=4c4f69&hide_border=true&cache_seconds=86400" alt="mooncell07/binar-river"> </a>
-    </div>
-
 </div>
+
+<style>
+    .cardDiv {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        justify-content: center;
+        gap: 5%;
+    }
+
+    .cardDiv img {
+        max-width: 100%;
+        height: auto;
+        transition: 0.5s ease;
+        cursor: pointer;
+    }
+
+    .cardDiv img:hover {
+        transform: scale(1.05);
+        box-shadow: 5px 5px 15px #181825;
+    }
+</style>
