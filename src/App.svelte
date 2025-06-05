4950143ab4e921109f2d03a86f76d86ed1db3b38
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2025-06-05 13:24:51 +0530

    Add landing page

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&family=Sansita+Swashed:wght@300..900&display=swap" rel="stylesheet">
+
+<html lang="en">
+<body>
+
+<div class="nav">
+
+<div class="nav-title"> Mooncell07 </div>
+
+</div>
+
+<div class="container">
+
+  <div class="container-title">Welcome!</div>
+  <div class="container-content"> Hey, this is nova/mooncell07, welcome to my personal blogging site. I developed this
+  site as part of my journey into Web Development. My main interests lie in Emulator
+  Development and Network Programming though i do enjoy exploring other areas in tech. I also love playing games, currently i am actively playing Warframe,
+  SCP: Roleplay and Fate/Grand Order.<br><br>
+
+  This site uses the Svelte UI framework and color scheme is a combination of Rose Pine and Catppuccin.
+  I think both the color schemes work well together. 
+  </div>
+
+  <div class="index">
+    <div class="index-entry">
+      <a href="uh"><div class="index-projects">Projects</div></a>
+      <div class="index-comment">(Some of my open source projects.)</div>
+    </div>
+
+    <div class="index-entry">
+      <a href="hu"><div class="index-posts">Posts</div></a>
+      <div class="index-comment">(Some mini-notes of what i did on a specific date.)</div>
+    </div>
+
+  </div>
+
+  <div class="container-content">
+
+  Any issues/pull requests to this site are welcomed.
+  You can find the repository for this site <a href="https://github.com/mooncell07/site">here</a>.<br>
+  
+  </div>
+  <img src="src/assets/red-panda-cute.png" alt="" width=100vh height=100vh style="margin-top: 2em;">
+  </div>
+
+  <footer>
+  © 2025 mooncell07
+  |
+  <a href="https://www.flaticon.com/free-stickers/red-panda" title="red panda stickers">Red panda stickers created by Surfsup.Vector - Flaticon</a>
+  </footer>
+
+</body>
+</html>
