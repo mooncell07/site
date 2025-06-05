e6749e2fc4697be218db4020b817a87ff957b181
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2025-06-05 14:59:56 +0530

    Fix max width for narrow screens

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,105 @@
 <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&family=Sansita+Swashed:wght@300..900&display=swap" rel="stylesheet">
 
+<style>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  font-weight: 400;
+  text-align: center;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.container-title {
+  font-size: 2.25em;
+}
+
+.container-content {
+  padding-top: 1em;
+  font-size: 1em;
+  max-width: 55%;
+}
+
+@media screen and (max-width: 768px) {
+  .container-content {
+    max-width: 100%;
+  }
+}
+
+.index {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--color-overlay);
+  width: 50%;
+  height: 25%;
+  margin-top: 2em;
+  margin-bottom: 2em;
+  border-radius: 10px;
+  position: relative;
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+}
+
+.index::after,
+.index::before {
+  content: "";
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-image: conic-gradient(from var(--angle), var(--color-nav1), var(--color-nav2), var(--color-nav1));
+  z-index: -1;
+  padding: 0.175em;
+  border-radius: 10px;
+  animation: 5s spin linear infinite;
+}
+
+.index-entry {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5em;
+}
+
+.index-projects {
+  background: radial-gradient(circle, var(--color-projects2), var(--color-projects1));
+  font-size: 1.5em;
+  font-weight: 600;
+  animation: scroll 2s linear infinite;
+  background-size: 200% 200%;
+  background-clip: text;
+  color: transparent;
+  text-decoration: underline;
+}
+
+.index-posts {
+  background: radial-gradient(circle, var(--color-posts1), var(--color-posts2));
+  font-size: 1.5em;
+  font-weight: 600;
+  animation: scroll 2s linear infinite;
+  background-size: 200% 200%;
+  background-clip: text;
+  color: transparent;
+}
+
+.index-comment {
+  color: var(--color-muted);
+  font-size: 1em;
+  font-style: italic;
+}
+
+</style>
+
 <html lang="en">
 <body>
 
 <div class="nav">
-
 <div class="nav-title"> Mooncell07 </div>
-
 </div>
 
 <div class="container">
@@ -43,7 +135,7 @@
   You can find the repository for this site <a href="https://github.com/mooncell07/site">here</a>.<br>
   
   </div>
-  <img src="src/assets/red-panda-cute.png" alt="" width=100vh height=100vh style="margin-top: 2em;">
+  <img src="assets/red-panda-cute.png" alt="" width=100vh height=100vh style="margin-top: 2em;">
   </div>
 
   <footer>
