a70bec68a0231d017175b7b2fc88f1b4b9ddf059
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2025-06-10 13:02:34 +0530

    add preliminary posts page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<style>
+  .error-container{
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex: 1;
+    
+  }
+  .error-content {
+    font-family: "Rubik", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 600;
+    font-style: normal;
+    font-size: clamp(0.5em, 2em, 2.5em);
+    padding-right: 0.2em;
+    margin-top: -1em;
+  }
+  status {
+    color: var(--color-nav1);
+    font-weight: 700;
+  }
+</style>
+
+
+<div class="error-container">
+
+  <img src="assets/red-panda-log.png" alt="" width=150vh height=150vh>
+  <div class="error-content">
+    <status>{page.status}</status>
+    {page.error.message}
+  </div>
+</div>
--- src/routes/Posts/+page.svelte
+++ src/routes/Posts/+page.svelte
@@ -1,7 +1,98 @@
 <script>
+
 	let { data } = $props();
+
+	function clean(line){
+		let struct = line.split(":", 2);
+		struct[0] = "<h1>"
+		return struct.join("");
+	}
+
 </script>
 
-<div class="container">
-{@html data.html}
+<style>
+	.posts-container{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-color: var(--color-base);
+		align-content: center;
+		height: 100vh;
+	}
+	.posts-main{
+		display: flex;
+		flex-direction: column;
+		align-items: left;
+		justify-content: center;
+		height: 100vh;
+		width: 50vw;
+		text-align: left;
+		background-color: var(--color-overlay);
+		flex: 1;
+}
+
+	.entry{
+		display: flex;
+		flex-direction: row;
+	}
+
+	.text {
+  	background-color: #313244;
+		color: var(--color-text);
+ 		padding: 0.75em;
+ 		border-radius: 10px;
+		margin-top: 1em;
+		height: max-content;
+
+		font-size: 0.25em;
+		font-weight: 300em;
+	}
+
+	pad {
+		width: 6vh;
+	}
+
+	img {
+		height: 6vh;
+		width: 6vh;
+	}
+
+  @media screen and (min-width: 768px) {
+    .text {
+      font-size: 0.5em;
+    }
+    img {
+    	height: 12vh;
+    	width: 12vh;
+    }
+    pad {
+    	width: 12vh
+    }
+}
+</style>
+
+<div class="posts-container">
+	<div class="posts-main">
+		{#each data.html as h, i}
+			{#if h.startsWith("<h1>")}
+				<div class="entry">
+
+					{#if i == (data.html.length-2)}
+					<img src="assets/red-panda-smiling.png" alt="">
+					{:else}
+					<pad></pad>
+					{/if}
+					
+					<div class="text">
+					{@html clean(h)}
+					</div>
+	
+				</div>
+			{/if}
+		{/each}
+
+	</div>
+
 </div>
+
